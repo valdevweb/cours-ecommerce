2f33e8fec10b4639cf88fee72e86e0fe33d6b700
valbtlec <34799033+valbtlec@users.noreply.github.com>
2022-07-17 11:41:14 +0200

    initial commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    public function index()
+    {
+        var_dump("ca marche");
+        die;
+    }
+    /**
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost", schemes={"http", "https"})
+     */
+    public function test(Request $request, $age)
+    {
+        return new Response("vous avez $age ans");
+    }
+}
